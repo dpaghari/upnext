0a4182ebe014454ef91749defa8a9ecc22589eab
dpaghari <dspagharion@gmail.com>
2016-09-10 07:27:08 +0800

    Added an input with google places autocomplete on map page

--- src/pages/EventMap.js
+++ src/pages/EventMap.js
@@ -5,22 +5,40 @@ import Header from '../components/Header';
 const GoogleMapsLoader = require('google-maps'); // only for common js environments
 GoogleMapsLoader.KEY = 'AIzaSyALT683365k3JbNnmRDLUNY-PfFEyJDKiM';
 GoogleMapsLoader.LIBRARIES = ['places'];
-
+let tr_g_u_loc;
 export default class EventMap extends React.Component {
 
   constructor(props) {
     super(props);
+    tr_g_u_loc = new Promise((resolve, reject) => {
+
+      let userCoords = {};
+      let userIP;
+      jsonp("http://jsonip.com/", function(res) {
+        userIP = res.ip;
+        jsonp("http://www.tigeoip.com/?token=8765FRCfs!87&ip=" + userIP, function(innerRes) {
+          userCoords.lat = innerRes.latitude;
+          userCoords.lng = innerRes.longitude;
+          userCoords.city = innerRes.city;
+          resolve(userCoords);
+        });
+      });
+    });
+
+
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition((pos)=> {
-      const { coords } = pos;
+    tr_g_u_loc.then((pos)=> {
+      const { lat, lng } = pos;
       GoogleMapsLoader.load(function(google) {
       new google.maps.Map(document.getElementById("EventMap"), {
-        center: {lat: coords.latitude, lng: coords.longitude},
+        center: {lat, lng},
         zoom: 12
       });
-    }, (error) => {
+
+      new google.maps.places.Autocomplete(this.refs.eventLocator);
+    }.bind(this), (error) => {
         console.log("error retrieving coordinates");
     });
 
@@ -33,9 +51,25 @@ export default class EventMap extends React.Component {
     return (
       <div id="EventMapView">
       <Header currentPage={appState.currentPage} dispatch={dispatch} />
+        <div class="eventLocator">
+          <input type="text" name="eventLocator" ref="eventLocator" />
+        </div>
         <div id="EventMap"></div>
       </div>
     );
   }
 
+}
+
+function jsonp(url, callback) {
+  var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+  window[callbackName] = function(data) {
+      delete window[callbackName];
+      document.head.removeChild(script);
+      callback(data);
+  };
+
+  var script = document.createElement('script');
+  script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+  document.head.appendChild(script);
 }
